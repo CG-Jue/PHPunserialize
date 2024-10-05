acfe43d9474f06dc7da60bffe732ef3fc4376a44
CG-Jue <cgjue871@163.com>
2024-10-05 21:42:57 +0800

    修改为一个基础的PHP结构

--- demo.php
+++ demo.php
@@ -1,60 +1,12 @@
 <?php
-highlight_file(__FILE__);
-error_reporting(0);
-
-class re{
-    public $chu0;
-    public function __toString(){
-        if(!isset($this->chu0)){
-            return "I can not believes!";
-        }
-        $this->chu0->$nononono;
+class leiming {
+    public $name;
+    public function __construct($name) {
+        $this->name = $name;
     }
 }
 
-class web {
-    public $kw;
-    public $dt;
-
-    public function __wakeup() {
-        echo "lalalla".$this->kw;
-    }
-
-    public function __destruct() {
-        echo "ALL Done!";
-    }
+if (isset($_GET['data'])) {
+    highlight_file(__FILE__);
+    unserialize(base64_decode($_GET['data']));
 }
-
-class pwn {
-    public $dusk;
-    public $over;
-
-    public function __get($name) {
-        if($this->dusk != "gods"){
-            echo "什么，你竟敢不认可?";
-        }
-        $this->over->getflag();
-    }
-}
-
-class Misc {
-    public $nothing;
-    public $flag;
-
-    public function getflag() {
-        eval("system('cat /flag');");
-    }
-}
-
-class Crypto {
-    public function __wakeup() {
-        echo "happy happy happy!";
-    }
-
-    public function getflag() {
-        echo "you are over!";
-    }
-}
-$ser = $_GET['ser'];
-unserialize($ser);
-?>
