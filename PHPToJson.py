6c6f0d4009c02a82cb79b06e6ce5426cbcb49bf6
CG-Jue <cgjue871@163.com>
2024-10-05 21:44:30 +0800

    PHP代码转AST并用json存储

--- PHPToJson.py
+++ PHPToJson.py
@@ -29,11 +29,16 @@ def export(items):
     return result  # 返回转换后的PHP代码元素列表
 
 def phpjson(filename):
+    # 创建一个解析器
     parse = make_parser()
+    # 打开文件
     with open(filename,'r') as inf:
+        # 打开输出文件
         with open('input1.json','w') as out:
             temp1 = inf.read() # 读取数据
             temp2 = parse.parse(temp1,lexer=lexer,tracking=with_lines) # 通过phply.phpprase 分析代码 形成ast树存储
             json.dump(export(temp2),out,indent=2)# json序列化存储到json文件中
     out.close()
     inf.close()
+
+phpjson('demo.php')
